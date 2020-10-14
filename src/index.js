ff53620e084d9a6c7b80b3b84e1cbd1be31b7647
Tamara <yaroslavcevatamara@gmail.com>
2020-10-14 23:32:30 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let specialChars = /[!@#$%^&*()[\],.?":{}|<>]/;
+  let newStr = str;
+  let newConfig = [];
+
+  // Search special characters in config and add backslash
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let bracketsPair = bracketsConfig[i];
+    newConfig[i] = [];
+
+    for (let j = 0; j < 2; j++) {
+
+      if (specialChars.test(bracketsPair[j])) {
+        newConfig[i][j] = `\\${bracketsPair[j]}`;
+      } else {
+        newConfig[i][j] = bracketsPair[j];
+      }
+
+    }
+  }
+
+  // Create regex for config
+
+  let stringConfig = newConfig.join('|').replace(/,/g, '');
+  let reg = new RegExp(stringConfig, 'g');
+
+  // Test regex
+
+  while (reg.test(newStr)) { newStr = newStr.replace(reg, ''); }
+
+  return newStr == '' ? true : false;
 }
